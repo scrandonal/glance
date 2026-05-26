f869bbc35c3d9d757f94304fd894ba5a3467f4d3
Sean Crandall <scrandonal@users.noreply.github.com>
2026-05-26 10:41:07 +0800

    feat: add main entry point with version flag and config path support

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/glanceapp/glance/internal/glance"
 )
 
+var (
+	// Version is set at build time via ldflags
+	Version = "dev"
+	// Commit is set at build time via ldflags
+	Commit = "none"
+)
+
 func main() {
-	os.Exit(glance.Main())
+	configPath := flag.String("config", "glance.yml", "Path to the configuration file")
+	showVersion := flag.Bool("version", false, "Print version information and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("glance %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
+
+	app, err := glance.New(*configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error initializing application: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := app.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running application: %v\n", err)
+		os.Exit(1)
+	}
 }
